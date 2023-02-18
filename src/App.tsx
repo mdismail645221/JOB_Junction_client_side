7aff32d45fb9dada3d162bae434155bcf5b0a4b7
Khalid Mim Muzahid <108398126+KhalidMimMuzahid@users.noreply.github.com>
2023-02-19 01:35:31 +0600

    Update App.tsx
    okk

--- src/App.tsx
+++ src/App.tsx
@@ -5,25 +5,6 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <div>
-        <h1>hello world</h1>
-        <h1>hello world</h1>
-        <h1>hello world</h1>
-        <h1>hello world</h1>
-        <h1>hello world</h1>
-        <h1>hello world</h1>
-        <h1>hello world</h1>
-        <h1>hello world</h1>
-        <h1>hello world</h1>
-        <h1>hello world</h1>
-        <h1>hello world</h1>
-        <h1>hello world</h1>
-        <h1>hello world</h1>
-        <h1>hello world</h1>
-        <h1>hello world</h1>
-        <h1>hello world</h1>
-      </div>
-        <h1>hello Bangladesh</h1>
     </div>
   );
 }
