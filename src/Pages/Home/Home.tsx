19e70085f644ea88c442750ccabb0dab3269a232
Mohammad Ismail Hossain <mdismail645221@gmail.com>
2023-02-20 16:21:17 +0600

    myNetwork component added & JobNewsFeed commponent added or StartingLoading components added

--- src/Pages/Home/Home.tsx
+++ src/Pages/Home/Home.tsx
@@ -1,11 +1,14 @@
 import React from "react";
 import NewsFeeds from "./NewsFeeds/NewsFeeds";
 import StartingLoading from '../../Components/StartingLoading/StartingLoading.js'
-
+import MyNetwork from '../MyNetwork/MyNetwork';
+import UserPost from '../Home/UserPost/UserPost'
 
 const Home = () => {
   return (
     <>
+      {/* <MyNetwork></MyNetwork>
+      <UserPost></UserPost> */}
       <NewsFeeds></NewsFeeds>
     </>
   );
