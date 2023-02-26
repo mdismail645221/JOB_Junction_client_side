26b2c0689a0429b360c14f0277ef7ce0328d1f7a
Khalid Mim Muzahid <khalidmimm@gmail.com>
2023-02-26 18:03:42 +0600

    job post form done

--- src/Pages/PostAJob/PostAJob.tsx
+++ src/Pages/PostAJob/PostAJob.tsx
@@ -1,77 +1,216 @@
-import React from "react";
-import { Box, Button, MenuItem, Paper, Select, Stack, TextField, Typography } from "@mui/material";
-import { NEXTBUTTON, POSTAJOBSCONTAINER, POSTCONTAINER } from "./PostAJob.styled";
+import React, { useContext } from "react";
+import {
+  Box,
+  Button,
+  MenuItem,
+  Paper,
+  Select,
+  Stack,
+  TextareaAutosize,
+  TextField,
+  Typography,
+} from "@mui/material";
+import {
+  NEXTBUTTON,
+  POSTAJOBSCONTAINER,
+  POSTCONTAINER,
+} from "./PostAJob.styled";
+import { SubmitHandler, useForm } from "react-hook-form";
+import moment from "moment";
+import { MyContext } from "../../context/MyProvider/MyProvider";
 
 const PostAJob = () => {
-
-
-  const handleWorkplace = (event: any) => {
-    console.log("event", event.target)
-
+  const { currentUser } = useContext(MyContext);
+  type Inputs = {
+    postDescription: string;
+    jobDescription: string;
+    companyName: string;
+    workplaceType: string;
+    jobLocation: string;
+    jobType: string;
+    jobTitle: string;
+    skillSetsString: string;
+    requireMentsString: string;
+    responsibilitiesString: string;
   };
+  const {
+    register,
+    handleSubmit,
+    watch,
+    reset,
+    formState: { errors },
+  } = useForm<Inputs>();
 
+  const handleFormSubmit: SubmitHandler<Inputs> = (data) => {
+    // console.log(data);
+
+    const {
+      companyName,
+      jobDescription,
+      jobLocation,
+      jobType,
+      jobTitle,
+      requireMentsString,
+      responsibilitiesString,
+      skillSetsString,
+      workplaceType,
+    } = data;
+    let responsibilities = responsibilitiesString.split("###");
+    let skillSets = skillSetsString.split("###");
+    let requireMents = requireMentsString.split("###");
+    const postDate = moment().format();
+    // console.log(postDescription, "\n", postDate);
+    const jobPost = {
+      userEmail: currentUser?.email,
+      jobTitle: jobTitle,
+      companyName: companyName,
+      jobDescription: jobDescription,
+      postDate: postDate,
+      skillSets: skillSets,
+      requireMents: requireMents,
+      responsibilities: responsibilities,
+      jobType: jobType,
+      workplaceType: workplaceType,
+      jobLocation: jobLocation,
+      questions: [],
+      allLikes: [],
+    };
+    console.log(jobPost);
+    //   todo: fetch here
+
+    // reset();
+  };
   return (
     <POSTAJOBSCONTAINER>
       <POSTCONTAINER>
-        <Stack direction='column' spacing={2}>
-          <Box>
-            <Typography className="title1" component='h3'>Find a great hire, fast</Typography>
-            <Typography className="title2" component="p">Rated #1 in increasing quality of hire.1</Typography>
-          </Box>
-          {/* job title section */}
-          <Stack>
-            <label className="labelTitle">Job title</label>
-            <TextField
-              label=""
-              id="outlined-size-small"
-              size="small"
-            />
-          </Stack>
-          {/* Company input field section */}
-          <Stack>
-            <label className="labelTitle">Company</label>
-            <TextField
-              label=""
-              id="outlined-size-small"
-              size="small"
-            />
-          </Stack>
-          {/* Workplace type input field section */}
-          <Stack>
-
-            <label className="labelTitle">Workplace type</label>
-            <select>
-              <option value="On site">On site</option>
-              <option value="Hybrid">Hybrid</option>
-              <option value="Remote">Remote</option>
-            </select>
-          </Stack>
-          {/* Job location input field section */}
-          <Stack>
-            <label className="labelTitle">Job location</label>
-            <TextField
-              label=""
-              id="outlined-size-small"
-              size="small"
-            />
-          </Stack>
-
-          {/* Job type select field section  */}
-          <Stack>
-            <label className="labelTitle">Job type</label>
-            <select>
-              <option value="Full Time">Full Time</option>
-              <option value="Part Time">Part Time</option>
-              <option value="Contract">Contract</option>
-              <option value="Volunteer">Volunteer</option>
-              <option value="Internship">Internship</option>
-            </select>
-          </Stack>
-          {/* next level button for description routes  */}.
-          <NEXTBUTTON>
-            <Button>Get Started for free</Button>
-          </NEXTBUTTON>
-
+        <Stack direction="column" spacing={2}>
+          <form onSubmit={handleSubmit(handleFormSubmit)}>
+            <Box>
+              <Typography className="title1" component="h3">
+                Find a great hire, fast
+              </Typography>
+              <Typography className="title2" component="p">
+                Rated #1 in increasing quality of hire.1
+              </Typography>
+            </Box>
+            {/* job title section */}
+            <Stack>
+              <label className="labelTitle">Job title</label>
+              <TextField
+                {...register("jobTitle", {
+                  required: "you must provide this field",
+                })}
+                label=""
+                id="outlined-size-small"
+                size="small"
+              />
+            </Stack>
+            <Stack>
+              <label className="labelTitle">Job Description</label>
+              <TextareaAutosize
+                {...register("jobDescription", {
+                  required: "you must provide this field",
+                })}
+                aria-label="minimum height"
+                minRows={5}
+                placeholder="write here the job description?"
+                style={{ width: "100%", paddingLeft: "10px" }}
+              />
+            </Stack>
+            {/* Company input field section */}
+            <Stack>
+              <label className="labelTitle">Company Name</label>
+              <TextField
+                {...register("companyName", {
+                  required: "you must provide this field",
+                })}
+                label=""
+                id="outlined-size-small"
+                size="small"
+              />
+            </Stack>
+            {/* requirments field section */}
+            <Stack>
+              <label className="labelTitle">Requirements</label>
+              <TextField
+                {...register("requireMentsString", {
+                  required: "you must provide this field",
+                })}
+                label=""
+                id="outlined-size-small"
+                size="small"
+                placeholder="separate them using ###"
+              />
+            </Stack>
+            {/* skillsets field section */}
+            <Stack>
+              <label className="labelTitle">Skill Sets</label>
+              <TextField
+                {...register("skillSetsString", {
+                  required: "you must provide this field",
+                })}
+                label=""
+                id="outlined-size-small"
+                size="small"
+                placeholder="separate them using ###"
+              />
+            </Stack>
+            {/* responsibilities field section */}
+            <Stack>
+              <label className="labelTitle">Responsibilities</label>
+              <TextField
+                {...register("responsibilitiesString")}
+                label=""
+                id="outlined-size-small"
+                size="small"
+                placeholder="separate them using ###"
+              />
+            </Stack>
+            {/* Workplace type input field section */}
+            <Stack>
+              <label className="labelTitle">Workplace type</label>
+              <select
+                {...register("workplaceType", {
+                  required: "you must provide this field",
+                })}
+              >
+                <option value="On site">On site</option>
+                <option value="Hybrid">Hybrid</option>
+                <option value="Remote">Remote</option>
+              </select>
+            </Stack>
+            {/* Job location input field section */}
+            <Stack>
+              <label className="labelTitle">Job location</label>
+              <TextField
+                {...register("jobLocation", {
+                  required: "you must provide this field",
+                })}
+                label=""
+                id="outlined-size-small"
+                size="small"
+              />
+            </Stack>
+            {/* Job type select field section  */}
+            <Stack>
+              <label className="labelTitle">Job type</label>
+              <select
+                {...register("jobType", {
+                  required: "you must provide this field",
+                })}
+              >
+                <option value="Full Time">Full Time</option>
+                <option value="Part Time">Part Time</option>
+                <option value="Contract">Contract</option>
+                <option value="Volunteer">Volunteer</option>
+                <option value="Internship">Internship</option>
+              </select>
+            </Stack>
+            {/* next level button for description routes  */}.
+            <NEXTBUTTON>
+              <Button type="submit">Get Started for free</Button>
+            </NEXTBUTTON>
+          </form>
         </Stack>
       </POSTCONTAINER>
     </POSTAJOBSCONTAINER>
