a864e4889733b91cd2f41b090b1c0c09391f8c69
Iktiup-Mahmud <seyam.official96@gmail.com>
2023-02-27 22:46:13 +0600

    change

--- src/layouts/Main/SearchActionType/DrawerForFilterAction/DrawerForFilterAction.tsx
+++ src/layouts/Main/SearchActionType/DrawerForFilterAction/DrawerForFilterAction.tsx
@@ -15,7 +15,6 @@ import { Link } from "react-router-dom";
 import { IconButton, Typography } from "@mui/material";
 import InputLabel from "@mui/material/InputLabel";
 import MenuItem from "@mui/material/MenuItem";
-import FormHelperText from "@mui/material/FormHelperText";
 import FormControl from "@mui/material/FormControl";
 import Select, { SelectChangeEvent } from "@mui/material/Select";
 import CloseIcon from "@mui/icons-material/Close";
@@ -24,79 +23,79 @@ import CloseIcon from "@mui/icons-material/Close";
 type Anchor = "right";
 
 const DrawerForFilterAction = (props: any) => {
-  // LIST
+    // LIST
 
-  const list = (anchor: Anchor) => (
-    <Box
-      sx={{ width: "500px" }}
-      role="presentation"
+    const list = (anchor: Anchor) => (
+        <Box
+            sx={{ width: "500px" }}
+            role="presentation"
 
-      //   onKeyDown={props.toggleDrawer(anchor, false)}
-    >
-      {/* jobs sections */}
-      <Box
-        sx={{
-          margin: "2rem 0",
-          display: "flex",
-          justifyContent: "space-between",
-          alignItems: "center",
-          paddingX: "1rem",
-        }}
-      >
-        {/* filter jobs section element */}
-        <Box>
-          <Typography component="p">
-            Filter only
-            <Select
-              sx={{ border: "none" }}
-              value={props?.state}
-              onChange={props?.handleChange}
-              displayEmpty
-              inputProps={{ "aria-label": "Without label" }}
-            >
-              <MenuItem value="">
-                <em>Jobs</em>
-              </MenuItem>
-              {/* <MenuItem value="jobs">Jobs</MenuItem> */}
-              <MenuItem value="people">People</MenuItem>
-              <MenuItem value="post">Posts</MenuItem>
-              <MenuItem value="companies">Companies</MenuItem>
-              <MenuItem value="groups">Groups</MenuItem>
-              <MenuItem value="events">Events</MenuItem>
-              <MenuItem value=" services">Services</MenuItem>
-            </Select>
-            <span>By</span>
-          </Typography>
-        </Box>
-
-        {/* Close btn start*/}
-        <Box>
-          <IconButton
-            // onKeyDown={props.toggleDrawer(anchor, false)}
-            onClick={props.toggleDrawer(anchor, false)}
-          >
-            <CloseIcon />
-          </IconButton>
-        </Box>
-        {/* Close btn end */}
-      </Box>
-      {/* to do : seyam vy start here */}
-    </Box>
-  );
-
-  return (
-    <Box sx={{ padding: "0px", position: "relative", top: "1000px" }}>
-      <React.Fragment>
-        <Drawer
-          anchor={"right"}
-          open={props.state["right"]}
-          //   onClose={props.toggleDrawer("right", false)}
+        //   onKeyDown={props.toggleDrawer(anchor, false)}
         >
-          {list("right")}
-        </Drawer>
-      </React.Fragment>
-    </Box>
-  );
+            {/* jobs sections */}
+            <Box
+                sx={{
+                    margin: "2rem 0",
+                    display: "flex",
+                    justifyContent: "space-between",
+                    alignItems: "center",
+                    paddingX: "1rem",
+                }}
+            >
+                {/* filter jobs section element */}
+                <Box>
+                    <Typography component="p">
+                        Filter only
+                        <Select
+                            sx={{ border: "none" }}
+                            value={props?.state}
+                            onChange={props?.handleChange}
+                            displayEmpty
+                            inputProps={{ "aria-label": "Without label" }}
+                        >
+                            <MenuItem value="">
+                                <em>Jobs</em>
+                            </MenuItem>
+                            {/* <MenuItem value="jobs">Jobs</MenuItem> */}
+                            <MenuItem value="people">People</MenuItem>
+                            <MenuItem value="post">Posts</MenuItem>
+                            <MenuItem value="companies">Companies</MenuItem>
+                            <MenuItem value="groups">Groups</MenuItem>
+                            <MenuItem value="events">Events</MenuItem>
+                            <MenuItem value=" services">Services</MenuItem>
+                        </Select>
+                        <span>By</span>
+                    </Typography>
+                </Box>
+
+                {/* Close btn start*/}
+                <Box>
+                    <IconButton
+                        // onKeyDown={props.toggleDrawer(anchor, false)}
+                        onClick={props.toggleDrawer(anchor, false)}
+                    >
+                        <CloseIcon />
+                    </IconButton>
+                </Box>
+                {/* Close btn end */}
+            </Box>
+
+        </Box>
+    );
+
+    return (
+        <Box sx={{ padding: "0px", position: "relative", top: "1000px" }}>
+            <React.Fragment>
+                <Drawer
+                    anchor={"right"}
+                    open={props.state["right"]}
+                //   onClose={props.toggleDrawer("right", false)}
+                >
+                    {list("right")}
+                </Drawer>
+            </React.Fragment>
+        </Box>
+    );
 };
 
 export default DrawerForFilterAction;
